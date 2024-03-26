38b71d26c45e104129ff46b7f7245ef2ac8efda9
fabT <fabien.varlet@live.fr>
2024-03-26 10:46:25 +0100

    Entrez le nombre d'équipes

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const form = document.querySelector("form")
+form.addEventListener("submit", handleForm)
+const input = document.querySelector("input")
+// input.addEventListener("input", console.log("input", input.value))
+const msgError = document.querySelector(".msgError")
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let numberOfTeams = 0
 
-setupCounter(document.querySelector('#counter'))
+function handleForm(e) {
+  e.preventDefault()
+  numberOfTeams = input.value
+  if (numberOfTeams < 3) {
+    msgError.style.display = "block"
+  } else {
+    msgError.style.display = "none"
+    teamsTable()
+  }
+  console.log("input", numberOfTeams)
+}
+
+function teamsTable() {
+  for (let i = 0; i < numberOfTeams; i++) {
+      const input = document.createElement("input")
+      console.log("hey");
+  }
+    
+}

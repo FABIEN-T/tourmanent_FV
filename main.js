174aa50356f5acd2336b05c4942728e5ba6b1c08
fabT <fabien.varlet@live.fr>
2024-03-27 13:31:43 +0100

    Affichage des rencontres avec inputs

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const formNumberOfTeams = document.querySelector(".formNumberOfTeams")
 formNumberOfTeams.addEventListener("submit", handleFormNumberOfTeams)
 const input = document.querySelector("input")
-// input.addEventListener("input", console.log("input", input.value))
 const msgError = document.querySelector(".msgError")
 
 let numberOfTeams = 0
@@ -15,7 +14,6 @@ function handleFormNumberOfTeams(e) {
     msgError.style.display = "none"
     teamsTable()
   }
-  console.log("input", numberOfTeams)
 }
 
 const inputTeamsContainer = document.querySelector(".inputTeamsContainer")
@@ -24,13 +22,18 @@ const btnPutTeams = document.querySelector(".btnPutTeams")
 
 function teamsTable() { 
   formNumberOfTeams.style.display = "none"
-  formNumberOfTeams.removeEventListener("input", handleFormNumberOfTeams)  
+  formNumberOfTeams.removeEventListener("input", handleFormNumberOfTeams) 
+  
+  inputTeamsContainer.style.display = "flex"
+
+
   
   for (let i = 0; i < numberOfTeams; i++) {  
     const divInputTeam = document.createElement("div")
     divInputTeam.classList.add("divInputTeam")
 
     const input = document.createElement("input")
+    input.classList.add("inputTeam")
     const team = document.createElement("p")
     team.textContent = `Equipe ${i + 1} :`
 
@@ -48,14 +51,87 @@ function teamsTable() {
   // creationOfDatingTables() 
 }
 
-function creationOfDatingTables() {
-  console.log("submit 0")
+function handleTeams(e) {
+  e.preventDefault()
+  const inputsTeams = document.querySelectorAll(".inputTeam")
+  const teamsArray = []
+  inputsTeams.forEach(input => teamsArray.push(input.value))
+
+  // console.log("inputsTeams", teamsArray)
   // const formPutTeams = document.querySelector(".formPutTeams")
   // formPutTeams.addEventListener("submit", handleTeams)
-  
+  creationOfMatchTables(teamsArray)
 }
 
-function handleTeams() {
-  e.preventDefault()
-  console.log("submit !!!")
+function creationOfMatchTables(teamsArray) {
+
+  
+  const numberOfMatchsPerSession = numberOfTeams - 2 
+  let tempValue = 0
+  let matchTables = []
+  // let copy = [...teamsArray]
+  // matchTables.push(copy)
+ 
+  // console.log("matchTables", matchTables)
+  // console.log("____________________")
+
+
+  for (let i = 0; i <= numberOfMatchsPerSession; i++)  {
+    tempValue = teamsArray[1]
+    teamsArray[1] = teamsArray[i+1]
+    teamsArray[i+1] = tempValue
+    // console.log("teamsArray ", teamsArray)
+    // console.log("____________________")
+    const copy = [...teamsArray]
+    // console.log(copy);
+
+    matchTables.push(copy.slice(0, 2))
+    matchTables.push(copy.slice(2))
+    // matchTables.push(copy)
+  }
+
+  // console.log("matchTables", matchTables)
+
+  // for (let i = 0; i < numberOfTeams - 1; i++) {
+  //   console.log(`J ${i+1}`, matchTables[i])    
+  //   console.log(`J ${i + 1}`, matchTables[i + 1]) 
+  //   console.log("____________________")
+  // }
+  
+  showMatchTables(matchTables)
+}
+
+function showMatchTables(matchTables) {
+  const formPutTeams = document.querySelector('.formPutTeams')
+  formPutTeams.style.display = "none"
+  formPutTeams.removeEventListener("submit", handleTeams)
+
+  const formResults = document.querySelector('.formResults')
+  formResults.style.display = "block"
+  console.log("SHOW", matchTables)
+
+  const day = document.createElement('div')
+  day.classList.add('day')
+    
+  let dayTitle = 1 
+  for (let j = 0; j < matchTables.length; j++) { 
+    console.log("matchTables", matchTables[j][0], matchTables[j][1])
+    const match = document.createElement('div')
+    match.classList.add('match')
+    console.log("%", j, j % 2)      
+    
+    match.innerHTML = `
+      ${j%2 === 0 ? `<p>Jounnée ${dayTitle}</p>` : ``}
+      <label for="score1">${matchTables[j][0]}</label>
+      <input type="number" id="score1">
+      <input type="number" id="score2">
+      <label for="score2">${matchTables[j][1]}</label>
+    `
+    j % 2 === 0 && dayTitle++
+    
+    day.appendChild(match)
+  }  
+    
+  formResults.appendChild(day) 
+  
 }
---
+++ rangement.js
@@ -0,0 +1,223 @@
+day.innerHTML = `
+<form class="formDay">
+<p>Journée ${i+1}</p>
+<div class="match">
+  <label for="score${i}">${matchTables[i][0]}</label>
+  <input type="number" id="score${i}">
+  <input type="number" id="score${i+1}">
+  <label for="score${i+1}">${matchTables[i][1]}</label>
+</div>
+<div class="match">
+  <label for="score${i+2}">${matchTables[i+1][0]}</label>
+  <input type="number" id="score1">
+  <input type="number" id="score1">
+  <label for="score${i+1}">${matchTables[i+1][1]}</label>
+</div>
+`
+
+// tempValue = teamsArray[1]
+  // teamsArray[1] = teamsArray[2]
+  // teamsArray[2] = tempValue
+  // console.log("teamsArray 2", teamsArray)
+  // copy = [...teamsArray]
+  // matchTables.push(copy)
+  // console.log("matchTables", matchTables)
+  // console.log("____________________")
+
+  // tempValue = teamsArray[1]
+  // teamsArray[1] = teamsArray[3]
+  // teamsArray[3] = tempValue
+  // console.log("teamsArray 3", teamsArray)
+  // copy = [...teamsArray]
+  // matchTables.push(copy)
+  // console.log("matchTables", matchTables)
+  // console.log("____________________")
+
+
+
+
+
+
+// function creationOfDatingTables(teamsArray) {  
+  
+//   let allMatchsArrray = []
+//   // console.log("teamsArray", teamsArray);
+
+//   for (let i = 0; i < teamsArray.length; i++) {
+//     for (let j = i + 1; j < teamsArray.length; j++) {
+//       let matchArray = []
+//       // console.log(teamsArray[i], teamsArray[j])
+//       matchArray.push(teamsArray[i])
+//       matchArray.push(teamsArray[j])
+//       allMatchsArrray.push(matchArray)
+//     }
+//   }  
+//   console.log("rencontres", allMatchsArrray)
+
+//   // nombre de matchs par session
+//   let numberOfMatchsPerSession = Math.trunc(numberOfTeams / 2)
+//   // console.log("numberOfMatchsPerSession", numberOfMatchsPerSession);
+  
+//   let allMatchsSessionArrray = []
+//   // console.log("JSON.stringify", JSON.stringify(allMatchsArrray[0]));
+//   // console.log(allMatchsArrray, allMatchsSessionArrray)
+//   // console.log("bool", (allMatchsArrray[0].filter(val => allMatchsArrray[5].includes(val))).length > 0)
+
+//   // allMatchsSessionArrray.push(allMatchsArrray[0])
+//   // for (let i = 0; i < allMatchsArrray.length; i++) {
+//   //   console.log(`allMatchsArrray${i}`, allMatchsArrray[i])
+//   //   // console.log("bool", (allMatchsArrray[i].filter(val => allMatchsArrray[i === allMatchsArrray.length-1 ? i-1 : i].includes(val))).length > 0)
+//   //   if ((allMatchsArrray[i].filter(val => allMatchsArrray[i === allMatchsArrray.length-1 ? i-1 : i].includes(val))).length > 0) {
+//   //     i++
+//   //   } else {
+//   //     allMatchsSessionArrray.push(allMatchsArrray[0])
+//   //   }
+//   // }
+
+//   // allMatchsSessionArrray.push(allMatchsArrray[0])
+
+//   // for (let i = 0; i <= allMatchsArrray.length; i++){
+//   //   console.log("i",i);
+//   //   if (
+//   //     allMatchsArrray[i][0] === allMatchsArrray[i + 1][0] ||
+//   //     allMatchsArrray[i][0] === allMatchsArrray[i + 1][1] ||
+//   //     allMatchsArrray[i][1] === allMatchsArrray[i + 1][0] ||
+//   //     allMatchsArrray[i][1] === allMatchsArrray[i + 1][1]
+//   //   ) {
+//   //     i++
+//   //   } else {
+//   //     allMatchsSessionArrray.push(allMatchsArrray[i + 1])
+//   // }
+    
+//   // }
+//   // console.log(allMatchsSessionArrray);
+
+
+//   // for (let i = 0; i < allMatchsArrray.length -1; i++) {
+    
+//   //   let session = []
+//   //   session.push(allMatchsArrray[i])
+//   //   let equalBool = allMatchsArrray[i][0] === allMatchsArrray[i + 1][0] ||  allMatchsArrray[i][0] === allMatchsArrray[i + 1][1] ||      allMatchsArrray[i][1] === allMatchsArrray[i + 1][0] ||     allMatchsArrray[i][1] === allMatchsArrray[i + 1][1]
+//   //   console.log(allMatchsArrray[i + 1][0]);
+//   //   console.log("equalBool", equalBool);
+//   //   if (equalBool) {
+//   //     i++
+//   //     session.push(allMatchsArrray[i + 1])
+//   //   }
+//   //   session.push(allMatchsArrray[i + 1])
+//   //   allMatchsSessionArrray.push(session)
+//   // }
+//   // console.log("Rencontres", allMatchsSessionArrray);
+
+
+
+
+
+
+//   // showAllMatch(allMatchsArrray)
+  
+// }
+
+// nombre triangulaire 1 3 6 10 15 28
+// function showAllMatch(allMatchsArrray) {
+//   console.log("R1", allMatchsArrray[0], allMatchsArrray[5])
+//   console.log("R2", allMatchsArrray[1], allMatchsArrray[4])
+//   console.log("R3", allMatchsArrray[2], allMatchsArrray[3])
+//   console.log("numberOfTeams", numberOfTeams)
+//   console.log("nb de sessions", allMatchsArrray.length);
+//   console.log("_________________________");
+  
+//   // if ()
+//   for (let i = 0; i < allMatchsArrray.length / 2; i++) {
+//     console.log(allMatchsArrray.length, allMatchsArrray.length % 2);
+//     console.log("i", i, allMatchsArrray.length -i -1);
+//     console.log("Sessions", allMatchsArrray[i], allMatchsArrray[allMatchsArrray.length -i -1])
+//   }
+  
+// }
+
+
+
+
+function showMatchTables(matchTables) {
+    const formPutTeams = document.querySelector('.formPutTeams')
+    formPutTeams.style.display = "none"
+    formPutTeams.removeEventListener("submit", handleTeams)
+  
+    const formResults = document.querySelector('.formResults')
+    formResults.style.display = "block"
+    console.log("SHOW", matchTables)
+    for (let i = 0; i < numberOfTeams - 1; i++) { // numberOfTeams - 1 = nombre de "journées de championnat"
+      const day = document.createElement('div')
+      day.classList.add('day')
+  
+      const matchTitle = document.createElement('p')
+      matchTitle.classList.add('matchTitle')    
+      matchTitle.textContent = `Jounnée ${i+1}`
+      day.appendChild(matchTitle)
+      
+      let h = i
+  
+      for (let j = 0; j < numberOfTeams - 2; j++) { // numberOfTeams - 2 = nombre de matchs par "journées de championnat"
+        h = i
+        console.log("j", j, "i", i, "h", h);
+        console.log("matchTables", matchTables[h+j][0], matchTables[h+j][1])
+        const match = document.createElement('div')
+        match.classList.add('match')
+  
+        match.innerHTML = `
+          <label for="score${j}">${matchTables[i][0]}</label>
+          <input type="number" id="score${j}">
+          <input type="number" id="score${j+1}">
+          <label for="score${j+1}">${matchTables[i][1]}</label>
+        `
+        day.appendChild(match)
+        
+      }
+      formResults.appendChild(day)  
+    }
+  }
+
+  
+<p>_______________________</p>
+  
+  for (let i = 0; i < numberOfTeams - 1; i++) { // numberOfTeams - 1 = nombre de "journées de championnat"
+    const day = document.createElement('div')
+    day.classList.add('day')
+
+    const matchTitle = document.createElement('p')
+    matchTitle.classList.add('matchTitle')    
+    matchTitle.textContent = `Jounnée ${i+1}`
+    day.appendChild(matchTitle)
+
+    for (let j = 0; j < matchTables.length; j++) { // numberOfTeams - 2 = nombre de matchs par "journées de championnat"
+   
+      // console.log("j", j, "i", i, "h", h);
+      console.log("matchTables", matchTables[j][0], matchTables[j][1])
+      const match = document.createElement('div')
+      match.classList.add('match')
+  
+      match.innerHTML = `
+        <label for="score1">${matchTables[j][0]}</label>
+        <input type="number" id="score1">
+        <input type="number" id="score2">
+        <label for="score2">${matchTables[j][1]}</label>
+      `
+      day.appendChild(match)
+      // formResults.appendChild(match) 
+    }
+    
+    formResults.appendChild(day)  
+  }
+
+
+
+
+
+
+
+
+
+
+
+ 
